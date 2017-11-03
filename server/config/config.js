8e91fbaa0c8494c4891232997206161fa0295339
valentineezeh <val.ezeh15@gmail.com>
2017-11-03 08:23:39 +0100

    Deploy to heroku

--- server/config/config.js
+++ server/config/config.js
@@ -14,18 +14,7 @@ module.exports = {
     dialect: 'postgres'
   },
   production: {
-    username: 'tfasxalepioaln',
-    password: '0d41dc6bb68407bb82cdaa68e924963ff26db15ec16459d7d9c74aa480cd87f2',
-    database: 'dfsm6g9e0ieuir',
-    host: 'ec2-54-235-150-134.compute-1.amazonaws.com',
-    dialect: 'postgres',
+    use_env_variables: 'DATABASE_URL',
   }
 };
 
-production: {
-  client: 'postgresql',
-  connection: {
-    database: 'my_db',
-    user:     'username',
-    password: 'password'
-  }
