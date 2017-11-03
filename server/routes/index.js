da2252dcfb06e9b903717a16ea01c365d90b8df2
valentineezeh <val.ezeh15@gmail.com>
2017-11-03 08:56:07 +0100

    Implement routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -9,7 +9,7 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res) => {
-  res.status(200).send({ message: 'More-Recipes' });
+  res.status(200).json({ message: 'More-Recipes' });
 });
 
 /* All API Routes */
